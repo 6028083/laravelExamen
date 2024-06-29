59bd4cb58d4462565f3142c2b2c59155a460a3f4
Burak <6028083@mborijnland.nl>
2024-06-29 11:48:38 +0200

    Alles werkend

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,12 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
-        \App\Models\Post::factory(2)->create();  // Voeg deze regel toe om ook posts te genereren
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Post::factory(10)->create();  // Voeg deze regel toe om ook posts te genereren
     }
 }
